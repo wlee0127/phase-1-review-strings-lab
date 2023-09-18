6215a920e8026ca4f7900711fb49347b8b3562a6
william lee <will.lee.npc@gmail.com>
2023-09-17 20:08:13 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = "Chi";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+
